691af3686b61f76bbd60da7ba301a04d21d1372d
Macey McCormick <red-rainbows@hotmail.com>
2020-01-29 12:28:17 -0500

    added exercise 3
    Instead of having it's own folder, it's a separate scene within exercise-2's folder!

---
+++ exercises/exercise_2/Assets/Build.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Build : MonoBehaviour
+{
+
+    public GameObject thecamera;
+    public GameObject create;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // count = count + 1;
+        // if (count >=4) count =0;
+        // && count == 0 in that if statement right below
+        // that slows the creation
+        if(Input.GetMouseButton(1))
+        {
+            RaycastHit hit;
+            Ray ray = thecamera.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                GameObject.Instantiate(create, hit.point, Quaternion.identity);
+            }
+        }
+    }
+}
