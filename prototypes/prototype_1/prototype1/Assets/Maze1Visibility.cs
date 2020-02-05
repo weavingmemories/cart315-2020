5e8dd19fd2f3cdcc02c01c3c969602e4760f1ecc
Macey McCormick <red-rainbows@hotmail.com>
2020-02-04 21:34:13 -0500

    finished prototype 1!
    yay!

---
+++ prototypes/prototype_1/prototype1/Assets/Maze1Visibility.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Maze1Visibility : MonoBehaviour
+{
+    int count;
+    public GameObject Maze1;
+    public GameObject Maze2;
+    public bool maze1Active = true;
+    public bool maze2Active = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MeshRenderer mr1 = Maze1.gameObject.GetComponent<MeshRenderer>();
+        mr1.enabled = true;
+        Collider cr1 = Maze1.gameObject.GetComponent<Collider>();
+        cr1.enabled = true;
+
+        MeshRenderer mr2 = Maze2.gameObject.GetComponent<MeshRenderer>();
+        mr2.enabled = false;
+        Collider cr2 = Maze2.gameObject.GetComponent<Collider>();
+        cr2.enabled = false;
+
+        maze1Active = true;
+        maze2Active = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+         count = count + 1;
+         if (count >=4) count =0;
+        if (Input.GetMouseButton(1) && count == 0)
+        {
+            if (maze1Active == true && maze2Active == false)
+            {
+                MeshRenderer mr1 = Maze1.gameObject.GetComponent<MeshRenderer>();
+                mr1.enabled = false;
+                Collider cr1 = Maze1.gameObject.GetComponent<Collider>();
+                cr1.enabled = false;
+
+                MeshRenderer mr2 = Maze2.gameObject.GetComponent<MeshRenderer>();
+                mr2.enabled = true;
+                Collider cr2 = Maze2.gameObject.GetComponent<Collider>();
+                cr2.enabled = true;
+
+                maze1Active = false;
+                maze2Active = true;
+            }
+            else if (maze1Active == false && maze2Active == true)
+            {
+                MeshRenderer mr1 = Maze1.gameObject.GetComponent<MeshRenderer>();
+                mr1.enabled = true;
+                Collider cr1 = Maze1.gameObject.GetComponent<Collider>();
+                cr1.enabled = true;
+
+                MeshRenderer mr2 = Maze2.gameObject.GetComponent<MeshRenderer>();
+                mr2.enabled = false;
+                Collider cr2 = Maze2.gameObject.GetComponent<Collider>();
+                cr2.enabled = false;
+
+                maze1Active = true;
+                maze2Active = false;
+            }
+        }
+    }
+
+  
+}
---
+++ prototypes/prototype_1/prototype1/Assets/Rotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+
+    public float rotate_speed = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+            this.transform.Rotate(0, rotate_speed, 0);
+
+    }
+}
